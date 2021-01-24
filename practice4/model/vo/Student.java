a3c5f68d5eb5bfe20fdcd0827f1b7cd3ed375bb4
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-24 15:51:19 +0900

    Add files via upload

---
+++ practice4/model/vo/Student.java
@@ -0,0 +1,29 @@
+package com.kh.example.practice4.model.vo;
+
+public class Student {
+	
+	private int grade;
+	private int classroom;
+	private String name;
+	private double height;
+	private char gender;
+	
+	{
+		grade = 2;
+		classroom = 6;
+		name = "장혜린";
+		height = 165.5;
+		gender = '여';
+	}
+	
+	public Student() {}
+	
+	public void information() {
+		System.out.println("학년: "+grade);
+		System.out.println("반: "+classroom);
+		System.out.println("이름: "+name);
+		System.out.println("키: "+height);
+		System.out.println("성별: "+gender);
+	}
+
+}
