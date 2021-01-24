d63d9c0c69fe7a2d266c429c27c03a37d9d69df1
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-24 15:44:22 +0900

    Add files via upload

---
+++ practice3/model/vo/Circle.java
@@ -0,0 +1,20 @@
+package com.kh.example.practice3.model.vo;
+
+public class Circle {
+	
+	private double PI = 3.14;
+	private int radius = 1;
+	
+	public void incrementRadius() {
+		radius += 1;
+	}
+	
+	public void getAreaOfCircle() {
+		System.out.println("원의 둘레: "+radius*2*PI);
+	}
+	
+	public void getSizeOfCircle() {
+		System.out.println("원의 넓이: "+radius*radius*PI);
+	}
+
+}
