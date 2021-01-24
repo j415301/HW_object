0cee1a1ade49728cd03f27778e0b1aff7996c16e
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-24 15:58:05 +0900

    Add files via upload

---
+++ practice5/model/vo/Lotto.java
@@ -0,0 +1,21 @@
+package com.kh.example.practice5.model.vo;
+
+public class Lotto {
+	
+	private int[] lotto = new int[6];
+	
+	{
+		for(int i=0 ; i<6 ; i++) {
+			lotto[i] = (int)(Math.random()*45)+1;
+		}
+	}
+	
+	public Lotto() {}
+	
+	public void information() {
+		for(int i=0 ; i<6 ; i++) {
+			System.out.print(lotto[i]+" ");
+		}
+	}
+ 
+}
